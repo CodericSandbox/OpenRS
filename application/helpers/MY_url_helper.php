90f2e9031117981949145576cbc3ec0166a74fda
OpenRS <help@openreviewscript.org>
2012-03-11 19:39:05 +0000

    fix url helper

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -1,16 +1,13 @@
 <?php
 	function url_title($str, $separator = 'dash', $lowercase = FALSE)
 	{
-//		if (UTF8_ENABLED)
-//		{
-//		        $CI =& get_instance();
-//		        $CI->load->helper('text');
-//echo "1".$str."<br>";
-//		        $str = utf8_decode($str);
-//echo "2".$str."<br>";
-//		        $str = preg_replace_callback('/(.)/', 'convert_accented_characters', $str);            
-//echo "3".$str."<br>";
-//		}
+		if (UTF8_ENABLED)
+	{
+		        $CI =& get_instance();
+		        $CI->load->helper('text');
+		        $str = utf8_decode($str);
+		        $str = preg_replace_callback('/(.)/', 'convert_accented_characters', $str);            
+		}
 		
 		if ($separator == 'dash')
 		{
@@ -34,19 +31,13 @@
 		);  
 
 		$str = strip_tags($str);
-//echo "4".$str."<br>";
-
 		foreach ($trans as $key => $val)
 		{
 			$str = preg_replace("#".$key."#iu", $val, $str);
 		}
-echo "5".$str."<br>";
-
 		if ($lowercase === TRUE)
 		{
 			$str = strtolower($str);
 		}
-echo "6".$str."<br>";
-
 		return trim(stripslashes($str));
 	}
